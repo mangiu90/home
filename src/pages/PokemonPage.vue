ed284aaa7d0bfc7330a496ab21c9fd36b8d9db0f
mangiu90 <giudicimanuel@gmail.com>
2022-08-29 18:36:59 -0300

    Update PokemonPage.vue

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,14 +1,14 @@
 <template>
     <h1 v-if="!pokemon">Espere por favor...</h1>
 
-    <div v-else class="grid grid-flow-col auto-cols-max gap-5">
+    <div v-else class="">
         
-        <PokemonPicture class=""/>
-        <PokemonOptions />
+        <PokemonPicture class="mb-3 mx-auto"/>
+        <PokemonOptions class="mb-3" />
 
-        <template v-if="showAnswer">
-            <h2 class="fade-in">{{ message }}</h2>
-            <button @click="newGame">
+        <template v-if="showAnswer" class="mb-3">
+            <h2 class="fade-in mb-3">{{ message }}</h2>
+            <button @click="newGame" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                 Nuevo Juego
             </button>
         </template>
